5061811a528facf65b69eab2bcfd2e44c9f8a945
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-05-24 17:15:10 +0300

    Домашнее задание 2-18. Создание API, Swagger UI, Postman (Maven) - начальный коммит

---
+++ src/main/java/ru/hogwarts/school/homework_2_18m/Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.homework_2_18m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
